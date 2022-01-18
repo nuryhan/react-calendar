4c59e30d078c56eb184144f04ee8def6257f64f7
nuryhan <nurmuhammet.muradov0309@gmail.com>
2022-01-18 15:36:12 +0500

    calendar app with reactjs

---
+++ src/components/CalendarHeader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CalendarHeader = ({onNext, onBack, dateDisplay}) => {
+    return (
+      <div id="header">
+        <div id="monthDisplay"> {dateDisplay} </div>
+        <div>
+          <button onClick={onBack} id="backButton">Back</button>
+          <button onClick={onNext} id="nextButton">Next</button>
+        </div>
+      </div>
+    );
+}
+
+export default CalendarHeader
---
+++ src/components/DeleteEventModal.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const DeleteEventModal = ({onDelete, eventText, onClose}) => {
+    return (
+      <>
+        <div id="deleteEventModal">
+          <h2>Event</h2>
+
+          <p id="eventText" > {eventText} </p>
+
+          <button onClick={onDelete}  id="deleteButton">Delete</button>
+          <button onClick={onClose} id="closeButton">Close</button>
+        </div>
+
+        <div id="modalBackDrop"></div>
+      </>
+    );
+}
+
+export default DeleteEventModal
